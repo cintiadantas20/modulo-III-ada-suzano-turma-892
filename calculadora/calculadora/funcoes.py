2aa6e800332c2966ef6998c0f9c5e44231b73f27
Cíntia <cintiamcdantas@gmail.com>
2022-11-15 21:38:00 -0300

    Altera funcoes

--- calculadora/calculadora/funcoes.py
+++ calculadora/calculadora/funcoes.py
@@ -9,7 +9,7 @@ def subtracao(a,b):
 def multiplicacao(a,b):
     mult = a * b
     return mult
-    
+
 def divisao(a, b):
     if b != 0:
         divisao = a / b
@@ -17,18 +17,3 @@ def divisao(a, b):
         print('Divisão inválida')
         divisao = 0
     return divisao
-
-def calcule():
-    a = int(input('Digite um número:'))
-    b = int(input('Digite outro número:'))
-    operacao = input('Digite uma operação:')
-    if operacao == 'soma'.lower() or '+':
-        resultado = soma(a, b)
-    elif operacao == 'subtracao'.lower() or '-':
-        resultado = subtracao(a, b)
-    elif operacao == 'multiplicacao'.lower() or '*':
-        resultado = multiplicacao(a, b)
-    elif operacao == 'divisao'.lower() or '/':
-        resultado = divisao(a, b)
-    print(resultado)
-    return resultado
