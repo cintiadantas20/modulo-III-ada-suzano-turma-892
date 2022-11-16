43bca5939b4b0afc5ab800819d2238669b68e63c
Cíntia <cintiamcdantas@gmail.com>
2022-11-15 21:13:59 -0300

    Cria funcao divisao

--- calculadora/calculadora/funcoes.py
+++ calculadora/calculadora/funcoes.py
@@ -5,3 +5,11 @@ def multiplicacao(a,b):
 def soma(a, b):
     soma = a + b
     return soma
+
+def divisao(a, b):
+    if b != 0:
+        divisao = a / b
+    else:
+        print('Divisão inválida')
+        divisao = 0
+    return divisao
